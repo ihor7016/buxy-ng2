ab0a099607bdacfb8202e76b197a1f5e71df527c
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-04-07 12:43:37 +0300

    drawer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {MatToolbarModule} from '@angular/material';
+import {MatIconModule} from '@angular/material';
+import {MatSidenavContainerModule} from '@angular/material';
+import {MatSidenavContentModule} from '@angular/material';
+import {MatNavListModule} from '@angular/material';
+import {MatSidenavModule} from '@angular/material';
+
+
+
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { DrawerComponent } from './drawer/drawer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    DrawerComponent
+
   ],
   imports: [
     MatToolbarModule,
-    BrowserModule
+    MatIconModule,
+    BrowserModule,
+    MatSidenavContainerModule,
+    MatNavListModule,
+    MatSidenavContentModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
