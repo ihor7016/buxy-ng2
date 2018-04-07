788408eaece43f18ddc94bcdc1c1ed557da41c21
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-04-07 12:06:56 +0300

    added toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import {MatToolbarModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
+    MatToolbarModule,
     BrowserModule
   ],
   providers: [],
