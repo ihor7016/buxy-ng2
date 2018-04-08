99671e8b736645dc6b7498a92b83521230559c68
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-04-08 12:11:57 +0300

    init dummy accounts

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-accounts',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountsComponent implements OnInit {
 
-  constructor() { }
+  accounts: Array;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.accounts = Array(5).fill(0).map((_, i) => `Account ${i + 1}`);
+  }
 }
