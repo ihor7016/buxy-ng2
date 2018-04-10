f0478b21db0526a02cf8c280270a700948e9e2af
Vladyslav <plakhutav@yahoo.com>
2018-04-10 16:09:30 +0200

    moved data array to class TableTransactionsComponent

--- src/app/table-transactions/table-transactions.component.ts
+++ src/app/table-transactions/table-transactions.component.ts
@@ -8,37 +8,27 @@ import { DataSource } from "@angular/cdk/table";
 })
 export class TableTransactionsComponent {
   displayedColumns = ["date", "amount", "description", "tag", "account"];
-  dataSource = ELEMENT_DATA;
+  dataSource = [
+    {
+      date: "19.03.2018",
+      amount: 200,
+      description: "tennis",
+      tag: "hobby",
+      account: "KredoBank, debitcard"
+    },
+    {
+      date: "22.03.2018",
+      amount: 5000,
+      description: "rent payment",
+      tag: "rent",
+      account: "PravexBank, creditcard"
+    },
+    {
+      date: "02.04.2018",
+      amount: 300,
+      description: "fishing",
+      tag: "hobby",
+      account: "KredoBank, debitcard"
+    }
+  ];
 }
-
-export interface Element {
-  date: string;
-  amount: number;
-  description: string;
-  tag: string;
-  account: string;
-}
-
-const ELEMENT_DATA: Element[] = [
-  {
-    date: "19.03.2018",
-    amount: 200,
-    description: "tennis",
-    tag: "hobby",
-    account: "KredoBank, debitcard"
-  },
-  {
-    date: "22.03.2018",
-    amount: 5000,
-    description: "rent payment",
-    tag: "rent",
-    account: "PravexBank, creditcard"
-  },
-  {
-    date: "02.04.2018",
-    amount: 300,
-    description: "fishing",
-    tag: "hobby",
-    account: "KredoBank, debitcard"
-  }
-];
