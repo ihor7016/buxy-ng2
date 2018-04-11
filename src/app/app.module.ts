c27b90de17c58ca79ffe9fb189b327fcf30e00e3
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-04-11 16:35:49 +0300

    delete drawer module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { MaterialComponentsModule } from "./material/material.module";
+import { AccountsModule } from "./accounts/accounts.module";
+import { TagsModule } from "./tags/tags.module";
 import { AppComponent } from "./app.component";
-import { DrawerModule } from "./drawer/drawer.module";
+import { DrawerComponent } from "./drawer/drawer.component";
+import { ContentComponent } from "./content/content.component";
+import { ToolbarComponent } from "./toolbar/toolbar.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    DrawerComponent,
+    ContentComponent,
+    ToolbarComponent
+  ],
   imports: [
     MaterialComponentsModule,
     BrowserModule,
     BrowserAnimationsModule,
-    DrawerModule
+    AccountsModule,
+    TagsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
