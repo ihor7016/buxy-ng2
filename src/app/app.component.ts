72a92291598c2afc0138757163a8121dd9b12796
gift-a <ksenia.gift@gmail.com>
2018-04-11 17:17:26 +0300

    make storage work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { StorageService } from "./services/storage.service";
+import { Observable } from "rxjs/Observable";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'app';
+  title: Observable<any>;
+  constructor(private storage: StorageService) {
+    this.title = this.storage.get();
+    this.storage.setUser(null, null);
+  }
 }
