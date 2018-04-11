ad4346009ae4356532bc867ed484951347e26df5
Vladyslav <plakhutav@yahoo.com>
2018-04-11 10:49:45 +0200

    bar chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { CdkTableModule } from "@angular/cdk/table";
 import { MatTableModule } from "@angular/material/table";
+import { ChartsModule } from "ng2-charts";
 
 import { AppComponent } from "./app.component";
 import { TableTransactionsComponent } from "./table-transactions/table-transactions.component";
+import { BarChartComponent } from "./bar-chart/bar-chart.component";
 
 @NgModule({
-  declarations: [AppComponent, TableTransactionsComponent],
+  declarations: [AppComponent, TableTransactionsComponent, BarChartComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     CdkTableModule,
-    MatTableModule
+    MatTableModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/bar-chart/bar-chart.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-bar-chart",
+  templateUrl: "./bar-chart.component.html",
+  styleUrls: ["./bar-chart.component.scss"]
+})
+export class BarChartComponent {
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartType: string = "bar";
+  public barChartLegend: boolean = true;
+
+  public barChartData: any[] = [
+    { data: [26500], label: "Income" },
+    { data: [11800], label: "Expenses" }
+  ];
+}
