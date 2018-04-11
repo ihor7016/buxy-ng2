61a263fee5071426730304987aee7b3af83157b8
gift-a <ksenia.gift@gmail.com>
2018-04-12 00:26:06 +0300

    tag dialog impl

--- src/app/tags/tags.module.ts
+++ src/app/tags/tags.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { TagsComponent } from "./tags.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { MaterialComponentsModule } from "../material/material.module";
 import { SharedModule } from "../shared/shared.module";
 
+import { TagsComponent } from "./tags.component";
+import { TagDialogComponent } from "./tag-dialog/tag-dialog.component";
+
 @NgModule({
-  imports: [CommonModule, MaterialComponentsModule, SharedModule],
-  declarations: [TagsComponent],
-  exports: [TagsComponent]
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MaterialComponentsModule,
+    SharedModule
+  ],
+  declarations: [TagsComponent, TagDialogComponent],
+  exports: [TagsComponent],
+  entryComponents: [TagDialogComponent]
 })
 export class TagsModule {}
