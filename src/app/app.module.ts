d188a1e06052546133e474002dbac84dd9c54c62
gift-a <ksenia.gift@gmail.com>
2018-04-13 17:47:24 +0300

    make layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,14 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MaterialComponentsModule } from "./material/material.module";
-import { AccountsModule } from "./accounts/accounts.module";
-import { TagsModule } from "./tags/tags.module";
-import { TransactionsModule } from "./transactions/transactions.module";
+import { LayoutModule } from "./layout/layout.module";
 
 import { AppComponent } from "./app.component";
-import { DrawerComponent } from "./drawer/drawer.component";
-import { ToolbarComponent } from "./toolbar/toolbar.component";
-import { AboutDialogComponent } from "./about-dialog/about-dialog.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DrawerComponent,
-    ToolbarComponent,
-    AboutDialogComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MaterialComponentsModule,
-    AccountsModule,
-    TagsModule,
-    TransactionsModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, LayoutModule],
   providers: [],
-  bootstrap: [AppComponent],
-  entryComponents: [AboutDialogComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+
+import { MaterialComponentsModule } from "../material/material.module";
+import { AccountsModule } from "../accounts/accounts.module";
+import { TagsModule } from "../tags/tags.module";
+import { TransactionsModule } from "../transactions/transactions.module";
+
+import { DrawerComponent } from "./drawer/drawer.component";
+import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { AboutDialogComponent } from "./about-dialog/about-dialog.component";
+
+@NgModule({
+  imports: [
+    MaterialComponentsModule,
+    AccountsModule,
+    TagsModule,
+    TransactionsModule
+  ],
+  declarations: [DrawerComponent, ToolbarComponent, AboutDialogComponent],
+  entryComponents: [AboutDialogComponent],
+  exports: [DrawerComponent]
+})
+export class LayoutModule {}
