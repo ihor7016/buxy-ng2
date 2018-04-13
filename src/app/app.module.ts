8e2469b83c6ad4c014028935eab19d92a13519fb
gift-a <ksenia.gift@gmail.com>
2018-04-13 18:51:08 +0300

    dumb not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+import { MaterialComponentsModule } from "./material/material.module";
 import { LayoutModule } from "./layout/layout.module";
+import { AccountsModule } from "./accounts/accounts.module";
+import { TagsModule } from "./tags/tags.module";
+import { TransactionsModule } from "./transactions/transactions.module";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, LayoutModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialComponentsModule,
+    AccountsModule,
+    TagsModule,
+    TransactionsModule,
+    LayoutModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from "@angular/core";
 
 import { MaterialComponentsModule } from "../material/material.module";
-import { AccountsModule } from "../accounts/accounts.module";
-import { TagsModule } from "../tags/tags.module";
-import { TransactionsModule } from "../transactions/transactions.module";
 
 import { DrawerComponent } from "./drawer/drawer.component";
 import { ToolbarComponent } from "./toolbar/toolbar.component";
 import { AboutDialogComponent } from "./about-dialog/about-dialog.component";
 
 @NgModule({
-  imports: [
-    MaterialComponentsModule,
-    AccountsModule,
-    TagsModule,
-    TransactionsModule
-  ],
+  imports: [MaterialComponentsModule],
   declarations: [DrawerComponent, ToolbarComponent, AboutDialogComponent],
   entryComponents: [AboutDialogComponent],
   exports: [DrawerComponent]
