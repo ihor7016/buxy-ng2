d01b3facc42b5df25587e41164d13f4c346d6ca7
gift-a <ksenia.gift@gmail.com>
2018-04-13 22:49:57 +0300

    Revert "dumb not working"
    This reverts commit 8e2469b83c6ad4c014028935eab19d92a13519fb.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,13 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MaterialComponentsModule } from "./material/material.module";
 import { LayoutModule } from "./layout/layout.module";
-import { AccountsModule } from "./accounts/accounts.module";
-import { TagsModule } from "./tags/tags.module";
-import { TransactionsModule } from "./transactions/transactions.module";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MaterialComponentsModule,
-    AccountsModule,
-    TagsModule,
-    TransactionsModule,
-    LayoutModule
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent]
 })
