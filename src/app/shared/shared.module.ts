609021333eb3aadeaab527e38da56a2c5d075f82
gift-a <ksenia.gift@gmail.com>
2018-04-13 23:42:01 +0300

    create confirm layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { ButtonMoreComponent } from "./button-more/button-more.component";
 import { MaterialComponentsModule } from "../material/material.module";
+
+import { ButtonMoreComponent } from "./button-more/button-more.component";
+import { ConfirmDialogComponent } from "./confirm-dialog/confirm-dialog.component";
 
 @NgModule({
   imports: [CommonModule, MaterialComponentsModule],
-  declarations: [ButtonMoreComponent],
-  exports: [ButtonMoreComponent]
+  declarations: [ButtonMoreComponent, ConfirmDialogComponent],
+  exports: [ButtonMoreComponent, ConfirmDialogComponent]
 })
 export class SharedModule {}
