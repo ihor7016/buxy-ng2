4d69367cc113f3b5e10929be42cbe759542a9f60
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-04-13 15:55:03 +0300

    clean

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,5 +1,5 @@
-import { MatDialog } from "@angular/material";
 import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material";
 import { TagDialogComponent } from "./tag-dialog/tag-dialog.component";
 
 @Component({
