e6f920799a079e3796390cb932d4cba55c8bad1b
Vladyslav <plakhutav@yahoo.com>
2018-04-14 22:02:59 +0200

    renamed charts.scss for chart.scss

--- src/app/transactions/bar-chart/bar-chart.component.ts
+++ src/app/transactions/bar-chart/bar-chart.component.ts
@@ -4,7 +4,7 @@ import { DataSource } from "@angular/cdk/table";
 @Component({
   selector: "app-bar-chart",
   templateUrl: "./bar-chart.component.html",
-  styleUrls: ["../../styles/charts.scss"]
+  styleUrls: ["../../styles/chart.scss"]
 })
 export class BarChartComponent {
   public barChartOptions: any = {
