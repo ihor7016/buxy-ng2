537a1e0ece6bef4aba0028c6140e5441f4fe444b
gift-a <ksenia.gift@gmail.com>
2018-04-17 18:00:02 +0300

    add checking for current account to edit

--- src/app/accounts/account-dialog/account-dialog.component.ts
+++ src/app/accounts/account-dialog/account-dialog.component.ts
@@ -48,11 +48,16 @@ export class AccountDialogComponent {
   }
 
   uniqueNameValidator(control: FormControl) {
-    const existing = this.data.accounts.reduce((res, acc) => {
-      return res
-        ? true
-        : control.value.toLowerCase() === acc.name.toLowerCase();
-    }, false);
-    return existing ? { existingName: { value: control.value } } : null;
+    let existing = true;
+    let val = control.value;
+    if (this.data.current) {
+      existing = !(this.data.current.name.toLowerCase() === val.toLowerCase());
+    }
+    if (existing) {
+      existing = this.data.accounts.reduce((res, acc) => {
+        return res ? true : acc.name.toLowerCase() === val.toLowerCase();
+      }, false);
+    }
+    return existing ? { existingName: { value: val } } : null;
   }
 }
