29ff8a675fb24982bbb458fd0e748ea809311b3e
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-04-17 10:50:35 +0300

    clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
 import { LayoutModule } from "./layout/layout.module";
 import { AuthModule } from "./auth/auth.module";
 import { AppRoutes } from "./app-routing.module";
+
 import { MainComponent } from "./main/main.component";
 import { AppComponent } from "./app/app.component";
 
--- src/app/transactions/transactions.module.ts
+++ src/app/transactions/transactions.module.ts
@@ -3,12 +3,12 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { MaterialComponentsModule } from "../material/material.module";
 import { ChartsModule } from "ng2-charts";
+import { SharedModule } from "../shared/shared.module";
 
 import { TransactionsComponent } from "./transactions/transactions.component";
 import { TransactionDialogComponent } from "./transaction-dialog/transaction-dialog.component";
 import { BarChartComponent } from "./bar-chart/bar-chart.component";
 import { TableTransactionsComponent } from "./table-transactions/table-transactions.component";
-import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [
