a2d7c617406fff51cd053b6b8535af70eeea6996
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-04-17 19:19:13 +0300

    position

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators
-} from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { AuthService } from "../../services/auth.service";
 
 @Component({
