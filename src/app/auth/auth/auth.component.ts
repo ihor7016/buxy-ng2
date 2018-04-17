91db512e81ac2f4b6211fb0da36202577a5a1468
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-04-17 18:49:36 +0300

    validation

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from "@angular/forms";
 import { AuthService } from "../../services/auth.service";
 
 @Component({
@@ -10,69 +15,32 @@ import { AuthService } from "../../services/auth.service";
 })
 export class AuthComponent implements OnInit {
   userForm: FormGroup;
-  formErrors = {
-    email: "",
-    password: ""
-  };
-  validationMessages = {
-    email: {
-      required: "Please enter your email",
-      email: "Please enter your vaild email"
-    },
-    password: {
-      required: "please enter your password",
-      pattern: "Password must contain numbers and letters",
-      minlength: "Please enter more than 4 characters",
-      maxlength: "Please enter less than 25 characters"
-    }
-  };
+  email: FormControl;
+  password: FormControl;
 
-  constructor(
-    private router: Router,
-    private fb: FormBuilder,
-    private auth: AuthService
-  ) {}
+  constructor(private router: Router, private auth: AuthService) {}
 
   ngOnInit() {
-    this.buildForm();
+    this.createFormControls();
+    this.createForm();
   }
 
-  buildForm() {
-    this.userForm = this.fb.group({
-      email: ["", [Validators.required, Validators.email]],
-      password: [
-        "",
-        [
-          Validators.pattern("^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$"),
-          Validators.minLength(6),
-          Validators.maxLength(25)
-        ]
-      ]
+  createFormControls() {
+    this.email = new FormControl("", [
+      Validators.required,
+      Validators.pattern("[^ @]*@[^ @]*")
+    ]);
+    this.password = new FormControl("", [
+      Validators.required,
+      Validators.minLength(8)
+    ]);
+  }
+
+  createForm() {
+    this.userForm = new FormGroup({
+      email: this.email,
+      password: this.password
     });
-
-    this.userForm.valueChanges.subscribe(data => this.onValueChanged(data));
-    this.onValueChanged();
-  }
-
-  onValueChanged(data?: any) {
-    if (!this.userForm) {
-      return;
-    }
-    const form = this.userForm;
-    for (const field in this.formErrors) {
-      if (Object.prototype.hasOwnProperty.call(this.formErrors, field)) {
-        this.formErrors[field] = "";
-        const control = form.get(field);
-        if (control && control.dirty && !control.valid) {
-          const messages = this.validationMessages[field];
-          for (const key in control.errors) {
-            if (Object.prototype.hasOwnProperty.call(control.errors, key)) {
-              this.formErrors[field] += messages[key] + " ";
-            }
-          }
-        }
-      }
-    }
   }
 
   signInWithGoogle() {
