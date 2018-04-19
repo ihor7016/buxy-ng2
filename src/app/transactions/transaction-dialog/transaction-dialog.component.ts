ef304bdfe96e7efc1e59ea55e48b7a7b77fc5382
gift-a <ksenia.gift@gmail.com>
2018-04-19 19:55:21 +0300

    add validation as in accounts

--- src/app/transactions/transaction-dialog/transaction-dialog.component.ts
+++ src/app/transactions/transaction-dialog/transaction-dialog.component.ts
@@ -1,7 +1,13 @@
-import { Component, Inject } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { Component, Inject, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, AbstractControl } from "@angular/forms";
 import { DatePipe } from "@angular/common";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { Transaction } from "../../interfaces/transaction";
+
+interface TransactionDialogData {
+  action: string;
+  dataToEdit?: Transaction;
+}
 
 @Component({
   selector: "app-transaction-dialog",
@@ -9,7 +15,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
   styleUrls: ["../../styles/dialog.scss"],
   providers: [DatePipe]
 })
-export class TransactionDialogComponent {
+export class TransactionDialogComponent implements OnInit {
   form: FormGroup;
 
   constructor(
@@ -17,8 +23,13 @@ export class TransactionDialogComponent {
     private datePipe: DatePipe,
     private matDialogRef: MatDialogRef<TransactionDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) {
+  ) {}
+
+  ngOnInit() {
     this.createForm();
+    this.form.controls.amount.valueChanges.subscribe(() =>
+      this.form.controls.amount.markAsTouched()
+    );
   }
 
   createForm() {
