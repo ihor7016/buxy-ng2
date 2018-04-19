a0147db80ea611d33dbec693d45d012fbc31e650
gift-a <ksenia.gift@gmail.com>
2018-04-19 21:29:45 +0300

    format

--- src/app/transactions/transaction-dialog/transaction-dialog.component.ts
+++ src/app/transactions/transaction-dialog/transaction-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
 import { DatePipe } from "@angular/common";
-import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
 import { Transaction } from "../../interfaces/transaction";
 
 interface TransactionDialogData {
