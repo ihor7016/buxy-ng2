a7f360ad6271a942156830e90752e15acc8a6b30
gift-a <gift-a@gmail.com>
2018-04-19 15:03:34 +0300

    make dialog

--- src/app/auth/auth-dialog/auth-dialog.component.ts
+++ src/app/auth/auth-dialog/auth-dialog.component.ts
@@ -1,52 +1,45 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { Component } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { MatDialog } from "@angular/material";
-import { AuthService } from "../../services/auth.service";
-import { AuthDialogComponent } from "../auth-dialog/auth-dialog.component";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
 
 @Component({
-  selector: "app-signin",
-  templateUrl: "./auth.component.html"
+  selector: "app-auth-dialog",
+  templateUrl: "./auth-dialog.component.html",
+  styleUrls: ["./auth-dialog.component.scss", "../../styles/dialog.scss"]
 })
-export class AuthComponent implements OnInit {
+export class AuthDialogComponent {
   userForm: FormGroup;
   email: FormControl;
   password: FormControl;
   error: string;
 
-  constructor(
-    private router: Router,
-    private auth: AuthService,
-    private dialog: MatDialog
-  ) {}
+  constructor(private matDialogRef: MatDialogRef<AuthDialogComponent>) {}
 
   ngOnInit() {
-    this.dialog.open(AuthDialogComponent);
-    // this.createFormControls();
-    // this.createForm();
+    this.createFormControls();
+    this.createForm();
   }
 
-  // createFormControls() {
-  //   this.email = new FormControl("", [
-  //     Validators.required,
-  //     Validators.pattern("[^ @]*@[^ @]*")
-  //   ]);
-  //   this.password = new FormControl("", [
-  //     Validators.required,
-  //     Validators.minLength(8)
-  //   ]);
-  // }
+  createFormControls() {
+    this.email = new FormControl("", [
+      Validators.required,
+      Validators.pattern("[^ @]*@[^ @]*")
+    ]);
+    this.password = new FormControl("", [
+      Validators.required,
+      Validators.minLength(8)
+    ]);
+  }
 
-  // createForm() {
-  //   this.userForm = new FormGroup({
-  //     email: this.email,
-  //     password: this.password
-  //   });
-  //   this.userForm.valueChanges.subscribe(str => {
-  //     this.error = "";
-  //   });
-  // }
+  createForm() {
+    this.userForm = new FormGroup({
+      email: this.email,
+      password: this.password
+    });
+    this.userForm.valueChanges.subscribe(str => {
+      this.error = "";
+    });
+  }
 
   // signInWithGoogle() {
   //   this.error = "";
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,8 +6,10 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { MaterialComponentsModule } from "../material/material.module";
 import { AngularFireDatabaseModule } from "angularfire2/database";
 import { AngularFireAuthModule } from "angularfire2/auth";
+
 import { AuthService } from "../services/auth.service";
 import { AuthComponent } from "./auth/auth.component";
+import { AuthDialogComponent } from "./auth-dialog/auth-dialog.component";
 import { AuthGuard } from "../services/auth-guard.service";
 import { config } from "../../../firebase.config";
 
@@ -22,6 +24,7 @@ import { config } from "../../../firebase.config";
   ],
   exports: [AuthComponent],
   providers: [AuthService, AuthGuard],
-  declarations: [AuthComponent]
+  declarations: [AuthComponent, AuthDialogComponent],
+  entryComponents: [AuthDialogComponent]
 })
 export class AuthModule {}
