28153c80b7f758cd39a080c7940e5ddcc7a0a762
gift-a <ksenia.gift@gmail.com>
2018-04-21 00:47:20 +0300

    try to add nested info

--- src/app/transactions/transactions/transactions.component.ts
+++ src/app/transactions/transactions/transactions.component.ts
@@ -4,10 +4,14 @@ import { TransactionDialogComponent } from "../transaction-dialog/transaction-di
 import { Observable } from "rxjs/Observable";
 
 import { TransactionsService } from "../../services/transactions.service";
+import { DatabaseService } from "../../services/database.service";
 
 import { Transaction } from "../../interfaces/transaction";
 import { Account } from "../../interfaces/account";
 import { Tag } from "../../interfaces/tag";
+
+import "rxjs/add/operator/mergeMap";
+import "rxjs/add/observable/from";
 
 @Component({
   selector: "app-transactions",
@@ -18,37 +22,36 @@ export class TransactionsComponent implements OnInit {
   transactions: Transaction[];
   accounts: Account[];
   tags: Tag[];
-  transactionsStream: Observable<Transaction[]>;
+  transactionsStream: Observable<any>;
 
   constructor(
     private dialog: MatDialog,
-    private transDB: TransactionsService
+    private transDB: TransactionsService,
+    private db: DatabaseService
   ) {}
   ngOnInit() {
-    this.transactionsStream = this.transDB.getList();
+    this.transactionsStream = this.transDB
+      .getList()
+      .mergeMap(list => {
+        let tableData: any = list;
+        return Observable.from(list).mergeMap(item => {
+          console.dir(item);
+          return (tableData.account = this.db.getData(
+            "accounts",
+            item.accountId
+          ));
+        });
+        // item.tag = this.db.getData("tags", item.tagId).subscribe();
+      })
+      .map(list => {
+        console.log(list);
+        return list;
+      });
     this.transDB.getList().subscribe(list => (this.transactions = list));
-    this.accounts = [
-      {
-        id: "id1",
-        name: "Privat",
-        balance: 500,
-        type: "savings",
-        currency: "€"
-      },
-      { id: "id2", name: "Cash", balance: 2000, type: "cash", currency: "₴" },
-      {
-        id: "id3",
-        name: "BoaBank",
-        balance: 5000,
-        type: "credit",
-        currency: "$"
-      }
-    ];
-    this.tags = [
-      { id: "id1", name: "Transport" },
-      { id: "id2", name: "Rent" },
-      { id: "id3", name: "Restaurant" }
-    ];
+    this.db
+      .getList("accounts")
+      .subscribe((list: Account[]) => (this.accounts = list));
+    this.db.getList("tags").subscribe((list: Tag[]) => (this.tags = list));
   }
 
   editTransaction() {
@@ -66,8 +69,20 @@ export class TransactionsComponent implements OnInit {
     });
     addTransactionDialog.afterClosed().subscribe(data => {
       if (data) {
-        this.transDB.setData(data);
+        this.transDB.setData(data).subscribe();
       }
     });
   }
 }
+
+export const ACC = [
+  { name: "Privat", balance: 500, type: "savings", currency: "€" },
+  { name: "Cash", balance: 2000, type: "cash", currency: "₴" },
+  { name: "BoaBank", balance: 5000, type: "credit", currency: "$" }
+];
+
+export const TAG = [
+  { name: "Transport" },
+  { name: "Rent" },
+  { name: "Restaurant" }
+];
