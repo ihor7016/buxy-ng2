0d15399ba65f7df01080a39616c1ceb02d4c1af7
gift-a <ksenia.gift@gmail.com>
2018-04-24 20:13:06 +0300

    format

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -11,27 +11,26 @@ import { AuthService } from "./auth.service";
 
 @Injectable()
 export class DatabaseService {
-  constructor(private db: AngularFireDatabase, private auth: AuthService) {}
-
-  private getPath() {
-    return this.auth.authState.map(user => `users/${user.uid}`);
+  path: Observable<string>;
+  constructor(private db: AngularFireDatabase, private auth: AuthService) {
+    this.path = this.auth.authState.map(user => `users/${user.uid}`);
   }
 
   getList<T>(dataType: string): Observable<T[]> {
-    return this.getPath().switchMap(path => {
+    return this.path.switchMap(path => {
       return this.db.list<T>(`${path}/${dataType}`).valueChanges();
     });
   }
 
   getData<T>(dataType: string, dataId: string): Observable<T> {
-    return this.getPath().switchMap(path => {
+    return this.path.switchMap(path => {
       return this.db.object<T>(`${path}/${dataType}/${dataId}`).valueChanges();
     });
   }
 
   setData(dataType: string, data: any): Observable<void> {
     const dataToStore = Object.assign(data);
-    const ref = this.getPath().switchMap(path =>
+    const ref = this.path.switchMap(path =>
       this.db.list(`${path}/${dataType}`).push(dataToStore)
     );
     return ref
@@ -40,13 +39,13 @@ export class DatabaseService {
   }
 
   updateData(dataType: string, data: any): Observable<void> {
-    return this.getPath().switchMap(path =>
+    return this.path.switchMap(path =>
       this.db.list(`${path}/${dataType}`).update(data.id, data)
     );
   }
 
   deleteData(dataType: string, dataId: string): Observable<void> {
-    return this.getPath().switchMap(path =>
+    return this.path.switchMap(path =>
       this.db.list(`${path}/${dataType}`).remove(dataId)
     );
   }
