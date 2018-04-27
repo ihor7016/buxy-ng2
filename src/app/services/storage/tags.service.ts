0449cd8c0c0ce04ae12271c1f4a135aa798c200b
gift-a <ksenia.gift@gmail.com>
2018-04-27 12:20:27 +0300

    format

--- src/app/services/storage/tags.service.ts
+++ src/app/services/storage/tags.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Tag } from "../interfaces/tag";
+import { Tag } from "../../interfaces/tag";
 import { DatabaseService } from "./database.service";
 import { AbstractService } from "./abstract.service";
 
--- src/app/services/stotage.module.ts
+++ src/app/services/stotage.module.ts
@@ -3,8 +3,14 @@ import { NgModule } from "@angular/core";
 import { DatabaseService } from "./storage/database.service";
 import { TransactionsService } from "./storage/transactions.service";
 import { AccountsService } from "./storage/accounts.service";
+import { TagsService } from "./storage/tags.service";
 
 @NgModule({
-  providers: [DatabaseService, TransactionsService, AccountsService]
+  providers: [
+    DatabaseService,
+    TransactionsService,
+    AccountsService,
+    TagsService
+  ]
 })
 export class StorageModule {}
