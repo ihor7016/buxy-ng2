13a6518f35422e24ce4c8459476e15a0c7375524
gift-a <ksenia.gift@gmail.com>
2018-04-27 12:53:01 +0300

    fix set, add condition on path

--- src/app/services/storage/database.service.ts
+++ src/app/services/storage/database.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { AngularFireDatabase } from "angularfire2/database";
 import { Observable } from "rxjs/Observable";
+import { fromPromise } from "rxjs/observable/fromPromise";
 import "rxjs/add/operator/switchMap";
 
 import { AuthService } from "../auth.service";
@@ -8,42 +9,69 @@ import { AuthService } from "../auth.service";
 @Injectable()
 export class DatabaseService {
   path: Observable<string>;
-
+  basePath: string;
   constructor(private db: AngularFireDatabase, private auth: AuthService) {
-    this.path = this.auth.authState.map(user => `users/${user.uid}`);
+    this.path = this.auth.authState.map(
+      user => (this.basePath = `users/${user.uid}`)
+    );
   }
 
   getList<T>(dataType: string): Observable<T[]> {
-    return this.path.switchMap(path => {
-      return this.db.list<T>(`${path}/${dataType}`).valueChanges();
-    });
+    console.log(this.basePath);
+    if (!this.basePath) {
+      return this.path.switchMap(path =>
+        this.db.list<T>(`${path}/${dataType}`).valueChanges()
+      );
+    }
+    return this.db.list<T>(`${this.basePath}/${dataType}`).valueChanges();
   }
 
   getData<T>(dataType: string, dataId: string): Observable<T> {
-    return this.path.switchMap(path => {
-      return this.db.object<T>(`${path}/${dataType}/${dataId}`).valueChanges();
-    });
+    if (!this.basePath) {
+      return this.path.switchMap(path => {
+        return this.db
+          .object<T>(`${path}/${dataType}/${dataId}`)
+          .valueChanges();
+      });
+    }
+    return this.db
+      .object<T>(`${this.basePath}/${dataType}/${dataId}`)
+      .valueChanges();
   }
 
   setData(dataType: string, data: any): Observable<void> {
-    const dataToStore = Object.assign(data);
-    const ref = this.path.switchMap(path =>
-      this.db.list(`${path}/${dataType}`).push(dataToStore)
+    const dataToStore = Object.assign({}, data);
+    const id = this.db.createPushId();
+    dataToStore.id = id;
+    if (!this.basePath) {
+      return this.path.switchMap(path =>
+        this.db.list(`${path}/${dataType}`).set(id, dataToStore)
+      );
+    }
+    return fromPromise(
+      this.db.list(`${this.basePath}/${dataType}`).set(id, dataToStore)
     );
-    return ref
-      .map(item => (dataToStore.id = item.key))
-      .switchMap(() => this.updateData(dataType, dataToStore));
   }
 
   updateData(dataType: string, data: any): Observable<void> {
-    return this.path.switchMap(path =>
-      this.db.list(`${path}/${dataType}`).update(data.id, data)
+    if (!this.basePath) {
+      return this.path.switchMap(path =>
+        this.db.list(`${path}/${dataType}`).update(data.id, data)
+      );
+    }
+    return fromPromise(
+      this.db.list(`${this.basePath}/${dataType}`).update(data.id, data)
     );
   }
 
   deleteData(dataType: string, dataId: string): Observable<void> {
-    return this.path.switchMap(path =>
-      this.db.list(`${path}/${dataType}`).remove(dataId)
+    if (!this.basePath) {
+      return this.path.switchMap(path =>
+        this.db.list(`${path}/${dataType}`).remove(dataId)
+      );
+    }
+    return fromPromise(
+      this.db.list(`${this.basePath}/${dataType}`).remove(dataId)
     );
   }
 }
