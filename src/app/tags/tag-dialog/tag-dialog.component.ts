0f50629101495b985b2161aa3bc2c33f6cce63f8
DESKTOP-03PPCU2\User <olgaviktorovnaorlova@gmail.com>
2018-05-02 10:53:13 +0300

    fix

--- src/app/tags/tag-dialog/tag-dialog.component.ts
+++ src/app/tags/tag-dialog/tag-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, AbstractControl } from "@angular/forms";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
 import { TagDialogData } from "./tag-dialog-data.interface";
+import { Tag } from "../../interfaces/tag.interface";
 
 @Component({
   selector: "app-tag-dialog",
