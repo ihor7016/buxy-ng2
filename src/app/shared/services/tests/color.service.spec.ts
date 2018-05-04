d08210b472343891f331cb9b970a901085f77d0b
gift-a <ksenia.gift@gmail.com>
2018-05-04 15:52:15 +0300

    color service test

---
+++ src/app/shared/services/tests/color.service.spec.ts
@@ -0,0 +1,26 @@
+import { TestBed, inject } from "@angular/core/testing";
+
+import { ColorService } from "../color.service";
+
+let service: ColorService;
+
+describe("ColorService", () => {
+  beforeAll(() => {
+    Math.random = () => 0.5;
+  });
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ColorService]
+    });
+    service = TestBed.get(ColorService);
+  });
+
+  it("should be created", () => {
+    expect(service).toBeTruthy();
+  });
+
+  it("should return rgb color #800000", () => {
+    expect(service.get()).toBe("#800000");
+  });
+});
