cf98981d550399a745091899499c8caa684307e5
gift-a <ksenia.gift@gmail.com>
2018-05-06 20:46:37 +0300

    impl test

--- src/app/layout/layout/layout.component.spec.ts
+++ src/app/layout/layout/layout.component.spec.ts
@@ -1,34 +1,49 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { MaterialComponentsModule } from "../../shared/material/material.module";
-
-import { AuthService } from "../../auth/auth/auth.service";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
 import { LayoutComponent } from "./layout.component";
-import { DrawerComponent } from "../drawer/drawer.component";
+import { AuthService } from "../../auth/auth/auth.service";
+import { User } from "@firebase/auth-types";
 
 describe("LayoutComponent", () => {
   let component: LayoutComponent;
   let fixture: ComponentFixture<LayoutComponent>;
-  let authSpy = jasmine.createSpyObj("AuthService", ["currentUser"]);
+  let authService;
+  let layout: HTMLElement;
+  class authMock {
+    currentUser = null;
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LayoutComponent, DrawerComponent],
-      imports: [MaterialComponentsModule],
-      providers: [{ provide: AuthService, useValue: authSpy }]
-    }).compileComponents();
-    authSpy = TestBed.get(AuthService);
+      declarations: [LayoutComponent],
+      providers: [{ provide: AuthService, useValue: authMock }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    });
+
     fixture = TestBed.createComponent(LayoutComponent);
     component = fixture.componentInstance;
+    authService = TestBed.get(AuthService);
+    layout = fixture.nativeElement;
+  });
+
+  it("should not have template content and user defined", () => {
+    expect(layout.textContent).toBe("");
+    expect(component.user).toBeUndefined();
+  });
+
+  it("should hide drawer when the user is null", () => {
+    authService.currentUser = null;
     fixture.detectChanges();
+    expect(layout.querySelector("app-drawer")).toBeNull();
+    expect(component.user).toBeNull();
   });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
-
-  xit("should load darwer when user is truthy", () => {
-    authSpy.currentUser.and.returnValue(true);
-    expect(component).toBeTruthy();
+  it("should show drawer when the user is not null", () => {
+    const user = { uid: "id" } as User;
+    authService.currentUser = user;
+    fixture.detectChanges();
+    expect(layout.querySelector("app-drawer")).toBeDefined();
+    expect(component.user).toBe(user);
   });
 });
