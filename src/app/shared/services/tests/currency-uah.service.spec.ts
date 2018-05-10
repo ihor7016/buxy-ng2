7622933e1028c8afd6a30832168fc158aea75e9d
gift-a <ksenia.gift@gmail.com>
2018-05-10 11:48:17 +0300

    format

--- src/app/shared/services/tests/currency-uah.service.spec.ts
+++ src/app/shared/services/tests/currency-uah.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from "@angular/core/testing";
 
-import { CurrencyUahService } from "..//currency-uah.service";
+import { CurrencyUahService } from "../currency-uah.service";
 
 describe("CurrencyUahService", () => {
   let service: CurrencyUahService;
